ab1a5fcb433d20a65b331cfc32a4b57a733be1af
vizeit <jigparekh@gmail.com>
2020-07-05 14:07:30 -0400

    1389. Create Target Array in the Given Order

---
+++ TargetArrInOrder.py
@@ -0,0 +1,9 @@
+def createTargetArray(nums, index):
+    rs = []
+    for i, j in zip(nums, index):
+        rs.insert(j, i)
+    return rs
+
+if __name__ == "__main__":
+    print(createTargetArray([0,1,2,3,4], [0,1,2,2,1]))
+    print(createTargetArray([1,2,3,4,0], [0,1,2,3,0]))
