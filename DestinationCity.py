d0aeb7ec4db810d6b268769e9dbea3dcbe9184d7
vizeit <jigparekh@gmail.com>
2020-07-11 16:26:04 -0400

    shorter source code

--- DestinationCity.py
+++ DestinationCity.py
@@ -3,11 +3,8 @@ def destCity(paths):
     dc = set()
     for path in paths:
         sc.add(path[0])
-        if path[1] not in sc:
-            dc.add(path[1])
-        if path[0] in dc:
-            dc.remove(path[0])
-    return dc.pop()
+        dc.add(path[1])
+    return (dc-sc).pop()
 if __name__ == "__main__":
     print(destCity([["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]))
     print(destCity([["B","C"],["D","B"],["C","A"]]))
