0f9fc861820f694f1efb6869159766ee8097eeda
vizeit <jigparekh@gmail.com>
2020-07-13 13:32:53 -0400

    approach 2 - list comprehension

--- CountNegativeInMatrix.py
+++ CountNegativeInMatrix.py
@@ -1,10 +1,6 @@
+from itertools import takewhile
 def countNegatives(grid):
-    ct = 0
-    for r in grid:
-        for i in range(len(r)-1, -1, -1):
-            if r[i] < 0: ct+=1
-            else: break
-    return ct
+    return sum(1 for r in grid for i in takewhile(lambda x: x<0, r[::-1]))
 if __name__ == "__main__":
     print(countNegatives([[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]))
     print(countNegatives([[3,2],[1,0]]))
