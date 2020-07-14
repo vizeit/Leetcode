764819992d3179ebaf7c702abf174140ae0aec44
vizeit <jigparekh@gmail.com>
2020-07-14 12:12:09 -0400

    1299. Replace e with Greatest e on Right Side

---
+++ ReplaceWithMaxOnRight.py
@@ -0,0 +1,8 @@
+def replaceElements(arr):
+    maxe=-1
+    for i in range(len(arr)-1, -1, -1):
+        arr[i], maxe = maxe, arr[i] if arr[i] > maxe else maxe
+    return arr
+
+if __name__ == "__main__":
+    print(replaceElements([1,2]))
