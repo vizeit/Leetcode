0798a85540a6b7bd9b7a22c7674003d5f0fae730
vizeit <jigparekh@gmail.com>
2020-07-22 13:44:20 -0400

    recursive approach

--- MergeTwoSortedLinkedLists.py
+++ MergeTwoSortedLinkedLists.py
@@ -5,28 +5,19 @@ class ListNode:
         self.next = next
 
 def mergeTwoLists(l1, l2):
-    front, back = None, None
-    while l1 or l2:
-        if l1 and l2:
-            if l1.val < l2.val:
-                mv = l1.val
-                l1 = l1.next
-            else: 
-                mv = l2.val
-                l2 = l2.next
-        elif l1:
-            mv = l1.val
-            l1 = l1.next
+    if l1 and l2:
+        if l1.val < l2.val:
+            l1.next = mergeTwoLists(l1.next, l2)
+            return l1
         else:
-            mv = l2.val
-            l2 = l2.next
-        if front is None and back is None:
-            front = ListNode(mv)
-            back = front
-        else:
-            back.next = ListNode(mv)
-            back = back.next
-    return front
+            l2.next = mergeTwoLists(l1, l2.next)
+            return l2
+    elif l1:
+        return l1
+    else:
+        return l2
+    
+    
 if __name__ == "__main__":
     l1 = ListNode(1, ListNode(2, ListNode(4)))
     l2 = ListNode(1, ListNode(3, ListNode(4)))
