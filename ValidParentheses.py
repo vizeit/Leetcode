a2722ffddf5cde029fe4e67282563e9755c1146a
vizeit <jigparekh@gmail.com>
2020-07-23 12:06:34 -0400

    20. Valid Parentheses

---
+++ ValidParentheses.py
@@ -0,0 +1,21 @@
+def isValid(s):
+    stack = []
+    for c in s:
+        if c == ')':
+            if not stack or '(' != stack.pop(): return False
+        elif c == ']':
+            if not stack or '[' != stack.pop(): return False
+        elif c == '}':
+            if not stack or '{' != stack.pop(): return False
+        else:
+            stack.append(c)
+    return True if not stack else False
+
+if __name__ == "__main__":
+    print(isValid("()"))
+    print(isValid("()[]{}"))
+    print(isValid("(]"))
+    print(isValid("([)]"))
+    print(isValid("{[]}"))
+    print(isValid("{"))
+    print(isValid("(("))
