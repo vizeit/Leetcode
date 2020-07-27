b76d7d829c97037803e4a2c2e63e8543d482b681
vizeit <jigparekh@gmail.com>
2020-07-27 15:41:55 -0400

    optimized - O(n/2)

--- InsertPosition.py
+++ InsertPosition.py
@@ -1,9 +1,16 @@
 def searchInsert(nums, target):
     if len(nums) == 0: return 0
-    for i in range(len(nums)):
-        if nums[i] >= target:
-            return i
-    return i+1
+    mid = len(nums) // 2
+    while 1:
+        if mid == len(nums) or nums[mid] == target: return mid
+        elif mid < 0 or (nums[mid] < target and (mid+1 == len(nums) or nums[mid+1] > target)): return mid+1
+        elif nums[mid] > target: mid -= 1
+        else: mid+=1
 
 if __name__ == "__main__":
+    print(searchInsert([1,3,4], 6))
     print(searchInsert([1,3,5,6], 5))
+    print(searchInsert([1,3,5,6], 2))
+    print(searchInsert([1,3,5,6], 7))
+    print(searchInsert([1,3,5,6], 0))
+    print(searchInsert([1,3,5,6,7,8,9,10,12,13,14], 2))
