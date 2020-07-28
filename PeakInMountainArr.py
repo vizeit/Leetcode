60e8c67e38d50a7f3a7543cb23d9ce0aa9cd620f
vizeit <jigparekh@gmail.com>
2020-07-27 21:23:18 -0400

    Binary search - O(log n)

--- PeakInMountainArr.py
+++ PeakInMountainArr.py
@@ -1,9 +1,19 @@
 def peakIndexInMountainArray(A):
     if len(A) < 2: return 0
-    for i in range(len(A)-1):
-        if A[i+1] <= A[i]:
-            return i
-    return i+1
+    low, high = 0, len(A)-1
+    while low <= high:
+        mid = (low+high)//2
+        if A[low] > A[low+1]:
+            return low
+        elif A[high] > A[high-1]:
+            return high
+        elif A[mid] > A[mid-1] and A[mid] > A[mid+1]:
+            return mid
+        elif A[mid] > A[mid-1] and A[mid] < A[mid+1]:
+            low = mid+1
+        else:
+            high = mid-1
+    return mid
 
 if __name__ == "__main__":
     try:
