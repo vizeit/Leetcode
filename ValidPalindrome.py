3be0453e51aef0fd38145663bb1dcfcfd66080c5
vizeit <jigparekh@gmail.com>
2020-08-03 23:52:27 -0400

    125. Valid Palindrome

---
+++ ValidPalindrome.py
@@ -0,0 +1,27 @@
+from unittest import TestCase, main
+
+class Solution:
+    def isPalindrome(self, s):
+        if not len(s): return True
+        start, end = 0, len(s)-1
+        while start < end:
+            while start < end and not s[start].isalnum(): start+=1
+            while start < end and not s[end].isalnum(): end-=1
+            if start < end and s[start].lower()!=s[end].lower(): return False
+            start, end = start+1, end-1
+        return True
+
+class testsolution(TestCase):
+    def setUp(self):
+        self.solution = Solution()
+        self.inou = [
+            ("A man, a plan, a canal: Panama", True),
+            ("race a car", False)
+        ]
+    def test_isPalindrome(self):
+        for p1, p2 in self.inou:
+            with self.subTest(input=p1, expected=p2):
+                self.assertEqual(self.solution.isPalindrome(p1), p2)
+
+if __name__ == "__main__":
+    main()
