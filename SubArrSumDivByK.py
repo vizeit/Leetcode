af7623b80fec6478e1c475d08ac466e550be0e63
vizeit <jigparekh@gmail.com>
2020-08-21 22:22:46 -0400

    One line solution

--- SubArrSumDivByK.py
+++ SubArrSumDivByK.py
@@ -1,14 +1,8 @@
-from collections import defaultdict
+from itertools import accumulate
+from collections import Counter
 class Solution(object):
     def subarraysDivByK(self, A, K):
-        d = defaultdict(int)
-        d[0] = 1
-        su = 0
-        for x in A:
-            su += x
-            d[su % K]+=1
-
-        return sum(v*(v-1)//2 for v in d.values())
+        return sum(v*(v-1)//2 for v in Counter(accumulate(A, lambda x,y: (x+y)%K, initial=0)).values())
 
 if __name__ == "__main__":
-    print(Solution().subarraysDivByK([4,5,0,-2,-3,1], 2))
+    print(Solution().subarraysDivByK([4,5,0,-2,-3,1], 5))
