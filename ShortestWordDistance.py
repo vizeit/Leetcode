60a292b2edb90bd988aacf3ba3c9694e0ee2ee8f
vizeit <jigparekh@gmail.com>
2020-08-27 13:42:55 -0400

    243. Shortest Word Distance

---
+++ ShortestWordDistance.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shortestDistance(self, words, word1, word2) -> int:
+        i1, i2, mindist = -1, -1, len(words)
+        for i, w in enumerate(words):
+            if w == word1: i1 = i
+            elif w == word2: i2 = i
+            if i1 != -1 and i2 != -1:
+                mindist = min(mindist, abs(i1-i2))
+        return mindist
+
+if __name__ == "__main__":
+    print(Solution().shortestDistance(["practice", "makes", "perfect", "coding", "makes"], "coding", "practice"))
+    print(Solution().shortestDistance(["practice", "makes", "perfect", "coding", "makes"], "coding", "makes"))
