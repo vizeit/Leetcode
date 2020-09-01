9ef9d0181ed70146fff6438e2f4ded98a01668b9
vizeit <jigparekh@gmail.com>
2020-09-01 15:56:07 -0400

    10. Regular Expression Matching

---
+++ RegularExpMatching.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp = [[False] * (len(p) + 1) for _ in range(len(s) + 1)]
+
+        dp[-1][-1] = True
+        for i in range(len(s), -1, -1):
+            for j in range(len(p) - 1, -1, -1):
+                first_match = i < len(s) and p[j] in {s[i], '.'}
+                if j+1 < len(p) and p[j+1] == '*':
+                    dp[i][j] = dp[i][j+2] or first_match and dp[i+1][j]
+                else:
+                    dp[i][j] = first_match and dp[i+1][j+1]
+
+        return dp[0][0]
+
+if __name__ == "__main__":
+    print(Solution().isMatch("aa","a"))
+    print(Solution().isMatch("aa","a*"))
+    print(Solution().isMatch("ab",".*"))
+    print(Solution().isMatch("aab","c*a*b"))
+    print(Solution().isMatch("mississippi","mis*is*p*."))
