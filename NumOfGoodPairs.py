23ff29fb4b9933957bc8fe46ea6e3350a996a856
vizeit <jigparekh@gmail.com>
2020-09-08 12:54:09 -0400

    more testcases

--- NumOfGoodPairs.py
+++ NumOfGoodPairs.py
@@ -12,7 +12,8 @@ class testsolution(TestCase):
             ([1,2,3,1,1,3], 4),
             ([1,1,1,1], 6),
             ([1,2,3], 0),
-            ([2,2,2], 3)
+            ([2,2,2], 3),
+            ([2,2,2,1,1], 4)
         ]
 
     def test_numIdenticalPairs(self):
